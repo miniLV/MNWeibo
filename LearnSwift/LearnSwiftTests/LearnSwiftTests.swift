559a58f7acb61fe7d4fb050f370c3ccb8a520a39
Tyrion Liang <tyrion.liang@ringcentral.com>
2019-04-22 13:59:51 +0800

    init Learn Swift Demo

---
+++ LearnSwift/LearnSwiftTests/LearnSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  LearnSwiftTests.swift
+//  LearnSwiftTests
+//
+//  Created by Tyrion Liang on 2019/4/22.
+//  Copyright © 2019 Tyrion Liang. All rights reserved.
+//
+
+import XCTest
+@testable import LearnSwift
+
+class LearnSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
