c614ea7bdaddb9895134b4bafa84da2aaa733b9a
miniLV <liangyuhangapple@gmail.com>
2020-03-10 22:05:06 +0800

    import icon & launch image

---
+++ MNWeibo/MNWeibo/Classes/View/Main/MNNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  MNNavigationController.swift
+//  MNWeibo
+//
+//  Created by miniLV on 2020/3/10.
+//  Copyright © 2020 miniLV. All rights reserved.
+//
+
+import UIKit
+
+class MNNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MNWeibo/MNWeibo/SceneDelegate.swift
+++ MNWeibo/MNWeibo/SceneDelegate.swift
@@ -17,7 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
         guard let _ = (scene as? UIWindowScene) else { return }
+        window = UIWindow()
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = MNMainTabBarController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
