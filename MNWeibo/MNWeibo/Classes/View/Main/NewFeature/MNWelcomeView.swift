15fff561d14b9b85c80b5f74d6a34031ae53ce84
miniLV <liangyuhangapple@gmail.com>
2020-03-22 23:50:49 +0800

    finish welcome view.

--- MNWeibo/MNWeibo/Classes/View/Main/NewFeature/MNWelcomeView.swift
+++ MNWeibo/MNWeibo/Classes/View/Main/NewFeature/MNWelcomeView.swift
@@ -7,16 +7,47 @@
 //
 
 import UIKit
+import SnapKit
+
+
 
 class MNWelcomeView: UIView {
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = UIColor.orange
+        
+        setupSubviews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupSubviews(){
+        
+        let backgroundImageView = UIImageView(image: UIImage(named: "ad_background"))
+        addSubview(backgroundImageView)
+        backgroundImageView.snp.makeConstraints {
+            $0.left.right.bottom.top.equalToSuperview()
+        }
+        
+        let avatarImageView = UIImageView(image: UIImage(named: "avatar_default_big"))
+        backgroundImageView.addSubview(avatarImageView)
+        avatarImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.size.equalTo(MNLayout.Layout(85))
+            $0.bottom.equalTo(-MNLayout.Layout(160))
+        }
+        
+        let label = UILabel()
+        label.text = "欢迎归来"
+        backgroundImageView.addSubview(label)
+        label.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo((avatarImageView.snp_bottomMargin)).offset(MNLayout.Layout(15))
+        }
+        
     }
 }
---
+++ MNWeibo/MNWeibo/MNLayout.swift
@@ -0,0 +1,46 @@
+//
+//  MNLayout.swift
+//  MNWeibo
+//
+//  Created by miniLV on 2020/3/22.
+//  Copyright © 2020 miniLV. All rights reserved.
+//
+
+import UIKit
+
+/**
+/**常用属性，这里就不抽出去了*/
+//屏幕宽高
+#define ScreenH  [[UIScreen mainScreen] bounds].size.height
+#define ScreenW  [[UIScreen mainScreen] bounds].size.width
+//创建frame
+#define Frame(x,y,width,height) CGRectMake(x, y, width, height)
+
+//图片方法(直接字符串赋值)
+#define MNImage(imgName)  [UIImage imageNamed:imgName]
+
+// iPhone X
+#define  LL_iPhoneX (ScreenW == 375.f && ScreenH == 812.f ? YES : NO)
+
+// Status bar height.
+#define  LL_StatusBarHeight      (LL_iPhoneX ? 44.f : 20.f)
+
+// Tabbar height.
+#define  DefaultBottomTabBarHeight         (LL_iPhoneX ? (49.f+34.f) : 49.f)
+
+// Status bar & navigation bar height.
+#define  DefaultNaviHeight  (LL_iPhoneX ? 88.f : 64.f)
+
+///默认间距 - 15
+#define DefaultMargin 15
+#define DefaultCellHeight 44
+*/
+
+struct MNLayout {
+
+    static let ratio:CGFloat = UIScreen.main.bounds.width / 375.0
+    
+    static func Layout(_ number: CGFloat) -> CGFloat {
+        return number * ratio
+    }
+}
